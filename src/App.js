a9fb34c638cd62d07be4b72e48f1e31d68966b8a
Stephen Moore <judo2000@comcast.net>
2022-02-25 14:20:32 -0700

    Add monster search/filter functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -17,9 +18,18 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <input
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
